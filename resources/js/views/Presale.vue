cf29c502d18eaddab7c01e8f256f14a6fa6f91c6
Steve Harmeyer <harmeyersteve@gmail.com>
2022-04-04 10:47:12 -0700

    gas fix

--- resources/js/views/Presale.vue
+++ resources/js/views/Presale.vue
@@ -117,13 +117,14 @@
 
             async function purchase() {
                 try {
-                    const gasPrice = Math.round(await web3.eth.getGasPrice() * 1.5);
-                    //let gas = Math.round(await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).estimateGas({ from: store.state.account, gasPrice: gasPrice }) * 1.5);
-                    //await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice, gas: gas });
-                    await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice });
-                    //gas = Math.round(await contract.value.methods.buy().estimateGas({ from: store.state.account, gasPrice: gasPrice}) * 1.5);
-                    //const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice, gas: gas });
-                    const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice });
+                    const gasPrice = 35000000000;
+                    //const gasPrice = Math.round(await web3.eth.getGasPrice() * 1.5);
+                    let gas = Math.round(await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).estimateGas({ from: store.state.account, gasPrice: gasPrice }) * 1.5);
+                    await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice, gas: gas });
+                    //await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice });
+                    gas = Math.round(await contract.value.methods.buy().estimateGas({ from: store.state.account, gasPrice: gasPrice}) * 1.5);
+                    const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice, gas: gas });
+                    //const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice });
                     console.log(result);
                 } catch (error) {
                     store.commit("alert", error.message);
