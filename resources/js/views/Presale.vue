da7012f65f966f9755a859eba0f6039bacb575ad
Steve Harmeyer <harmeyersteve@gmail.com>
2022-04-04 11:34:36 -0700

    gas fix

--- resources/js/views/Presale.vue
+++ resources/js/views/Presale.vue
@@ -118,12 +118,12 @@
             async function purchase() {
                 try {
                     const gasPrice = await web3.eth.getGasPrice();
-                    const gasLimit = await web3.eth.getGasLimit();
+                    const gasLimit = Math.round(await web3.eth.getGasLimit() * 2);
                     alert(gasLimit);
-                    //let gas = Math.round(await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).estimateGas({ from: store.state.account, gasPrice: gasPrice }) * 2);
-                    await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice, gasLimit: gasLimit });
-                    //gas = Math.round(await contract.value.methods.buy().estimateGas({ from: store.state.account, gasPrice: gasPrice}) * 2);
-                    const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice, gasLimit: gasLimit });
+                    let gas = Math.round(await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).estimateGas({ from: store.state.account, gasPrice: gasPrice }) * 2);
+                    await paymentContract.value.methods.approve(store.state.presaleNftAddress, price.value).send({ from: store.state.account, gasPrice: gasPrice, gasLimit: gasLimit, gas: gas });
+                    gas = Math.round(await contract.value.methods.buy().estimateGas({ from: store.state.account, gasPrice: gasPrice}) * 2);
+                    const result = await contract.value.methods.buy().send({ from: store.state.account, gasPrice: gasPrice, gasLimit: gasLimit, gas: gas });
                     console.log(result);
                 } catch (error) {
                     store.commit("alert", error.message);
