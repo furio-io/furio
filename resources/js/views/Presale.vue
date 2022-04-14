586a0b273dfddb5cba6128d952808ca20ad18f16
Steve Harmeyer <harmeyersteve@gmail.com>
2022-04-13 20:38:27 -0700

    hide quantity if only available option is 1

--- resources/js/views/Presale.vue
+++ resources/js/views/Presale.vue
@@ -18,7 +18,8 @@
         </div>
         <div v-show="store.state.address.attributes.email && store.state.address.attributes.email_verified_at">
             <div v-show="max">
-                <input v-model="quantity" :disabled="locked" :max="max" min="1" type="number" class="form-control mb-2" id="quantity">
+                <input v-show="max > 1" v-model="quantity" :disabled="locked" :max="max" min="1" type="number" class="form-control mb-2" id="quantity">
+                <input v-model="quantity" type="hidden">
                 <button @click="purchase" :disabled="locked" class="btn btn-lg btn-primary">Purchase ({{ totalPrice / 1000000 }} USDC)</button>
             </div>
             <div v-show="!max">
